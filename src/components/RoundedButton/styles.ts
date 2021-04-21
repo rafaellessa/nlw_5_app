706d7f96ddb1e34952c14ece475af3690e4a054c
Rafael Lessa <rafael.lessa@basesoft.com.br>
2021-04-21 14:38:01 -0300

    format

--- src/components/RoundedButton/styles.ts
+++ src/components/RoundedButton/styles.ts
@@ -1,8 +1,8 @@
-import styled from 'styled-components/native'
-import colors from '../../utils/colors'
+import styled from "styled-components/native";
+import colors from "../../utils/colors";
 
 export const Container = styled.TouchableOpacity.attrs({
-  activeOpacity: 0.7
+  activeOpacity: 0.7,
 })`
   background-color: ${colors.green};
   justify-content: center;
@@ -10,4 +10,4 @@ export const Container = styled.TouchableOpacity.attrs({
   border-radius: 16px;
   margin-bottom: 10px;
   height: 56px;
-`
+`;
